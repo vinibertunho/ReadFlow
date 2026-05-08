92b5499c5514f48e2963030fe8325d2badbc18dc
Isabella <isabella.cabrera@aluno.senai.br>
2026-05-08 08:34:30 -0300

    feat: finaliza o controller de personagens.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import livrosRoutes from './routes/livroRoute.js';
+import personagemRoutes from './routes/personagemRoute.js'
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,6 +14,7 @@ app.get('/', (req, res) => {
 
 // Rotas
 app.use('/api/livro', livrosRoutes);
+app.use('/api/personagem', personagemRoutes);
 
 app.use((req, res) => {
     res.status(404).json({ error: 'Rota não encontrada' });
